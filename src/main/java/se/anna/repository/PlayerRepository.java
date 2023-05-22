39db5c6c7b2f219fcacfdc764d424df43ff2d492
Anna Axelsson <a.axelsson51@gmail.com>
2023-05-22 17:50:25 +0200

    Add player repository

---
+++ src/main/java/se/anna/repository/PlayerRepository.java
@@ -0,0 +1,17 @@
+package se.anna.repository;
+
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import se.anna.business.Player;
+import se.anna.business.PlayerAverage;
+
+import java.util.List;
+import java.util.Scanner;
+
+@Repository
+public class PlayerRepository {
+
+}
