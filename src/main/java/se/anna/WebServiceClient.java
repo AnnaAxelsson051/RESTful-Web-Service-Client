d38392e58f23c89f2e70ab67a91dcee18b0faed3
Anna Axelsson <a.axelsson51@gmail.com>
2023-05-22 17:46:00 +0200

    Add web service client

---
+++ src/main/java/se/anna/WebServiceClient.java
@@ -0,0 +1,13 @@
+package se.anna;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServiceClient {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServiceClient.class, args);
+    }
+
+}
