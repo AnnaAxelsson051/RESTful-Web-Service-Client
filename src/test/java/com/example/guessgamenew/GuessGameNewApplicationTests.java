8024399a661e2a251a285ad464891263388753b9
Anna Axelsson <a.axelsson51@gmail.com>
2023-05-22 17:52:25 +0200

    Add files

---
+++ src/test/java/com/example/guessgamenew/GuessGameNewApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.guessgamenew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GuessGameNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
